75b92faa9840c6038c047ef8ea3a227e5ee46588
Chris Chou <chris.chou.c@nycu.edu.tw>
2022-05-31 20:35:43 +0800

    Pick from 32bf20f (origin/cy)

---
+++ Assets/Scripts/IconPointer.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class IconPointer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateObjInformation();
+
+
+        if (isclicked)
+        {
+            clicktime += Time.deltaTime;
+        }
+        else 
+        {
+            clicktime = 0;
+        }
+    }
+
+    public delegate void ButtonClick(int val);
+    public ButtonClick buttonClicked;
+
+    [SerializeField]
+    GameObject _obj;
+    Vector3 _localPosition;
+    Quaternion _localRotation;
+    Vector3 _localScale;
+    [SerializeField] // tmp
+    int _idx;
+
+
+    [Header("Control")]
+    float clicktime;
+    bool isclicked;
+
+    public IconPointer(GameObject obj)
+    {
+        _obj = obj;
+        UpdateObjInformation();
+    }
+
+    public void Setobj(GameObject obj)
+    {
+        _obj = obj;
+        UpdateObjInformation();
+    }
+
+    public GameObject Getobj()
+    {
+        return _obj;
+    }
+
+    public void Setidx(int idx)
+    {
+        _idx = idx;
+    }
+
+    public int Getidx()
+    {
+        return _idx;
+    }
+
+    public void SetUsing(bool bo)
+    {
+        
+        _obj.SetActive(bo);
+    }
+    void UpdateObjInformation()
+    {
+        if (_obj == null)
+        {
+            return;
+        }
+        _localPosition = _obj.transform.localPosition;
+        _localRotation = _obj.transform.localRotation;
+        _localScale = _obj.transform.localScale;
+    }
+
+    private void OnMouseDown()
+    {
+        isclicked = true;
+
+    }
+    private void OnMouseUp()
+    {
+        isclicked = false;
+        if (clicktime < 0.25f)
+        {
+            Debug.Log("IconPointer is Clicked");
+            buttonClicked(_idx);
+        }
+        else 
+        {
+            float dis = ToolBoxManager.toolBoxManager.GetDistance_IconPointer() * Screen.width;
+            float posX = ToolBoxManager.toolBoxManager.GetMinBorder_IconPointer() * Screen.width +_idx * dis;
+            Vector2 pos = new Vector2(posX, this.gameObject.GetComponent<RectTransform>().anchoredPosition.y);
+            this.gameObject.GetComponent<RectTransform>().anchoredPosition = pos;
+        }
+    }
+
+    private void OnMouseDrag() 
+    {
+        //Debug.Log("IconPonter is Draged");
+        float posY = this.gameObject.GetComponent<RectTransform>().anchoredPosition.y;
+        float posX = Input.mousePosition.x - Screen.width/2;
+        float dis = ToolBoxManager.toolBoxManager.GetDistance_IconPointer() * Screen.width;
+        float minX = ToolBoxManager.toolBoxManager.GetMinBorder_IconPointer() * Screen.width - dis / 2f;
+        float maxX = ToolBoxManager.toolBoxManager.GetMaxBorfer_IconPointer() * Screen.width + dis / 2f;
+        
+        if (posX < minX )
+        {
+            
+            posX = minX ;
+        }
+        else if (posX > maxX)
+        {
+            posX = maxX ;
+        }
+        Vector2 pos = new Vector2(posX, posY);
+        this.gameObject.GetComponent<RectTransform>().anchoredPosition = pos;
+        
+    }
+
+    public bool isBottonClicked()
+    {
+        return isclicked;
+    }
+
+
+    
+}
---
+++ Assets/Scripts/ToolBoxManager.cs
@@ -0,0 +1,183 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+public class ToolBoxManager : MonoBehaviour
+{
+    //加入xyz微調    
+    //新增上方圈圈的拖動
+    //物件更新時改變上方圈圈資訊
+    [SerializeField]
+    GameObject toolBox;
+    [SerializeField]            //tmp
+    GameObject curTarget;
+    int curIconIndex;
+
+    [SerializeField]
+    GameObject Icon_Arrow;
+    UnityAction myUnityAction_Arrow;
+    [SerializeField]
+    GameObject Template_Arrow;
+    
+
+    GameObject Icon_TextBox;
+    GameObject Icon_Cancel;
+
+    [SerializeField]
+    List<GameObject> myIconPointerList;
+    float minBorder_IconPointer;
+    float maxBorder_IconPointer;
+    float curmaxBorder_IconPointer;
+    float distance_IconPointer;
+    [SerializeField]
+    GameObject Template_IconPointer_Arrow;
+
+    
+
+    public static ToolBoxManager toolBoxManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (toolBoxManager == null)
+        {
+            toolBoxManager = this;
+            
+        }
+        else 
+        {
+            Destroy(this);
+        }
+
+        Icon_SetEventClick();
+        minBorder_IconPointer = -400f/1920f;
+        curmaxBorder_IconPointer = minBorder_IconPointer;
+
+        maxBorder_IconPointer = 650f/1920f;
+        distance_IconPointer = 150f / 1920f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIndex_IconPointer();
+    }
+
+    public void SetToolBoxVisible(bool isvisible)
+    {
+        toolBox.SetActive(isvisible);
+        
+        
+    }
+
+    public void SetCurTarget(GameObject target)
+    {
+        curTarget = target;
+    }
+
+    void Icon_SetEventClick()
+    {
+        myUnityAction_Arrow += ArrowIconClick;
+        Icon_Arrow.GetComponent<ToolBoxIcon>().AddEventClick(myUnityAction_Arrow);
+
+        
+    }
+
+    void ArrowIconClick()
+    {
+        GameObject tmp_icon = GameObject.Instantiate(Template_IconPointer_Arrow, Template_IconPointer_Arrow.transform.parent);
+
+        tmp_icon.transform.localPosition = Template_IconPointer_Arrow.transform.localPosition + distance_IconPointer * myIconPointerList.Count * Screen.width * Vector3.right;//改初始位置
+        tmp_icon.SetActive(true);
+
+        GameObject tmp = GameObject.Instantiate(Template_Arrow,curTarget.transform);
+        tmp.transform.localPosition = Vector3.zero;
+        tmp.SetActive(true);
+
+        tmp_icon.GetComponent<IconPointer>().Setobj(tmp);
+        tmp_icon.GetComponent<IconPointer>().Setidx(myIconPointerList.Count);
+        tmp_icon.GetComponent<IconPointer>().buttonClicked += ShowIconPointerInList;
+        curmaxBorder_IconPointer += distance_IconPointer ;
+        if (curmaxBorder_IconPointer > maxBorder_IconPointer)
+        {
+            curmaxBorder_IconPointer = maxBorder_IconPointer;
+        }
+
+        myIconPointerList.Add(tmp_icon);
+        ShowIconPointerInList(myIconPointerList.Count-1);
+        
+    }
+    void ShowIconPointerInList(int val)
+    {
+        if(myIconPointerList.Count == 0)
+        {
+            return;
+        }
+        for(int i=0; i<myIconPointerList.Count ;i++)
+        {
+            if(i != val)
+            {
+                myIconPointerList[i].GetComponent<IconPointer>().SetUsing(false);
+            }
+            else{
+                myIconPointerList[i].GetComponent<IconPointer>().SetUsing(true);
+                curIconIndex = i;
+            }
+        }
+    }
+
+    public float GetMinBorder_IconPointer()
+    {
+        return minBorder_IconPointer;
+    }
+    public float GetMaxBorfer_IconPointer()
+    {
+        return curmaxBorder_IconPointer;
+        ;
+    }
+
+    public float GetDistance_IconPointer()
+    {
+        return distance_IconPointer;
+    }
+
+    void CheckIndex_IconPointer()
+    {
+        if (myIconPointerList.Count < 2)
+        {
+            return;
+        }
+        for (int i = 0; i < myIconPointerList.Count-1; i++)
+        {
+            int j = i + 1;
+            if (myIconPointerList[i].GetComponent<RectTransform>().anchoredPosition.x > myIconPointerList[j].GetComponent<RectTransform>().anchoredPosition.x)
+            {
+                GameObject tmp = myIconPointerList[i];
+                if (!myIconPointerList[i].GetComponent<IconPointer>().isBottonClicked())
+                {
+                    Vector2 pos = myIconPointerList[j].GetComponent<RectTransform>().anchoredPosition;
+                    pos.x = minBorder_IconPointer * Screen.width + j * distance_IconPointer * Screen.width;
+                    myIconPointerList[i].GetComponent<RectTransform>().anchoredPosition = pos;
+                }
+
+
+                if (!myIconPointerList[j].GetComponent<IconPointer>().isBottonClicked())
+                {
+                    Vector2 pos = myIconPointerList[i].GetComponent<RectTransform>().anchoredPosition;
+                    pos.x = minBorder_IconPointer * Screen.width + i * distance_IconPointer * Screen.width;
+                    myIconPointerList[j].GetComponent<RectTransform>().anchoredPosition = pos;
+                }
+
+                myIconPointerList[i] = myIconPointerList[j];
+                myIconPointerList[i].GetComponent<IconPointer>().Setidx(i);
+                myIconPointerList[j] = tmp;
+                myIconPointerList[j].GetComponent<IconPointer>().Setidx(j);
+            }
+            
+        }
+    }
+
+
+}
